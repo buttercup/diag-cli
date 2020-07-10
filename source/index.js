362b499d07f6ca54937c67eda410c25122754018
Perry Mitchell <perry@perrymitchell.net>
2020-07-11 00:00:05 +0300

    Add export

--- source/index.js
+++ source/index.js
@@ -1,17 +1,47 @@
 #!/usr/bin/env node
 
+const fs = require("fs");
+const pify = require("pify");
 const pw = require("pw");
 const { Credentials, FileDatasource, Vault, createVaultFacade, init } = require("buttercup");
+const { exportVaultToCSV } = require("@buttercup/exporter");
 const { table } = require("table");
 const chalk = require("chalk");
 const jsonSize = require("json-size");
 
+const writeFile = pify(fs.writeFile);
+
 const [command, ...args] = process.argv.slice(2);
+
+async function exportToCSV() {
+    const [filename, target] = args;
+    if (!filename) {
+        throw new Error("Vault file must be provided");
+    } else if (!target) {
+        throw new Error("Target file must be provided");
+    }
+    process.stdout.write("Password: ");
+    const password = await getPassword();
+    const { vault } = await getVault(filename, password);
+    const csv = await exportVaultToCSV(vault);
+    await writeFile(target, csv);
+    console.log(`Wrote: ${target}`);
+}
 
 function getPassword() {
     return new Promise(resolve => {
         pw(password => resolve(password));
     });
+}
+
+async function getVault(filename, password) {
+    const fds = new FileDatasource(Credentials.fromDatasource({
+        type: "file",
+        path: filename
+    }, password));
+    const { Format, history } = await fds.load(Credentials.fromPassword(password));
+    const vault = Vault.createFromHistory(history, Format);
+    return { datasource: fds, vault };
 }
 
 async function inspect() {
@@ -21,12 +51,7 @@ async function inspect() {
     }
     process.stdout.write("Password: ");
     const password = await getPassword();
-    const fds = new FileDatasource(Credentials.fromDatasource({
-        type: "file",
-        path: filename
-    }, password));
-    const { Format, history } = await fds.load(Credentials.fromPassword(password));
-    const vault = Vault.createFromHistory(history, Format);
+    const { vault } = await getVault(filename, password);
     const vaultFacade = createVaultFacade(vault);
     // Form report data
     const data = [];
@@ -40,6 +65,8 @@ async function inspect() {
 async function run() {
     if (command === "inspect") {
         return inspect();
+    } else if (command === "export") {
+        return exportToCSV();
     } else {
         throw new Error(`Unknown command: ${command}`);
     }
